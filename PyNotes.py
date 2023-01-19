138b15486cc87feb86e6f8c426b548fbafad5eda
dhruvsinghal7494 <122340013+dhruvsinghal7494@users.noreply.github.com>
2023-01-19 21:51:21 +0530

    Create PyNotes.py

---
+++ PyNotes.py
@@ -0,0 +1,40 @@
+#print("hello world")
+
+#name = "Dhruv"
+#age = 19
+#print(name)
+#print(age)
+
+#name = "Adi"
+#age = 20.0
+
+#is_adult = True
+#print(name)
+
+
+#First_name = Tony
+#last_name = Stark
+#age = 51
+#is_genius = True
+
+#concatenation
+#name = input("What is your name? ")
+#print("Hello " + name)
+
+
+#input Function
+old_age = input("what is your age? ")
+new_age = int(old_age) + 2
+print(float(new_age))
+
+
+#type conversion
+int()
+float()
+str()
+bool()
+
+
+
+
+
