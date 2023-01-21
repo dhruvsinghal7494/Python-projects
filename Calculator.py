947879bd2e4fabf9a0fe6a7536c776e5cc689c58
Shubham Patel <shubhamprecious1@gmail.com>
2023-01-21 17:28:38 +0530

    Updated code
    No need too convert answer to string. This way is less code writing required

--- Calculator.py
+++ Calculator.py
@@ -4,16 +4,17 @@ Second = int(input("What is the Second No.? "))
 
 if Operator == "+":
     Result = First + Second
-    print("The Solution is " + str(Result))
+    print("The Solution is ", Result)
 elif Operator == "-":
     Result = First - Second
-    print("The Solution is " + str(Result))
+    print("The Solution is ", Result)
 elif Operator == "*":
     Result = First * Second
-    print("The Solution is " + str(Result))
+    print("The Solution is ", Result)
 elif Operator == "/":
     Result = First / Second
-    print("The Solution is " + str(Result))
+    print("The Solution is ", Result)
 elif Operator == "%":
     Result = First % Second
-    print("The Solution is" + str(Result))
+
+print("The Solution is", Result)
